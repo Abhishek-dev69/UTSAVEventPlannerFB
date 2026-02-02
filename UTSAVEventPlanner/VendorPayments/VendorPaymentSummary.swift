15fb81598e1110a3e20ca54dda2ea6e199982162
Abhishek <abhishek12439525@gmail.com>
2026-02-02 11:29:06 +0530

    Changes made in the payment summary

---
+++ UTSAVEventPlanner/VendorPayments/VendorPaymentSummary.swift
@@ -0,0 +1,16 @@
+//
+//  VendorPaymentSummary.swift
+//  UTSAV
+//
+//  Created by Abhishek on 02/02/26.
+//
+
+import Foundation
+
+struct VendorPaymentSummary {
+    let vendorId: String
+    let vendorName: String
+    let totalPayable: Double
+    let totalPaid: Double
+}
+
