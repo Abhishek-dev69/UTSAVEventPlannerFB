42182c51a947d34913e1ebd3eae2b7fb46624760
Abhishek <abhishek12439525@gmail.com>
2026-03-26 18:06:05 +0530

    UI changes

--- UTSAVEventPlanner/TabbarNEW/Payments/PaymentsEventsListViewController.swift
+++ UTSAVEventPlanner/TabbarNEW/Payments/PaymentsEventsListViewController.swift
@@ -32,7 +32,8 @@ final class PaymentsEventsListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .clear
+        view.backgroundColor = .systemBackground
+        applyBrandGradient()
         setupTable()
         setupKeyboardDismissTap()
 
